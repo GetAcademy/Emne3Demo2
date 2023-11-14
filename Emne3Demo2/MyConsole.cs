9a8563680ed9b95bd51f30f9ecaaaebcf2072938
GET Academy <terje@kolderup.net>
2023-11-14 11:25:52 +0100

    Initial commit

---
+++ Emne3Demo2/MyConsole.cs
@@ -0,0 +1,24 @@
+﻿namespace Emne3Demo2
+{
+    internal class MyConsole
+    {
+        public static string Ask(string question)
+        {
+            Console.Write(question);
+            var answer = Console.ReadLine();
+            return answer;
+        }
+
+        public static int AskForInt(string question)
+        {
+            var answer = Ask(question);
+            return Convert.ToInt32(answer);
+        }
+
+        public static bool AskForBool(string question)
+        {
+            var answer = Ask(question);
+            return answer.ToLower() == "j";
+        }
+    }
+}
---
+++ Emne3Demo2/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace Emne3Demo2
+{
+    class Program
+    {
+        // function main(arguments)
+        static void Main(string[] arguments)
+        {
+            var name = MyConsole.Ask("Hva heter du? ");
+            Console.WriteLine($"Hei, {name}!");
+
+            var birthYear = MyConsole.AskForInt("I hvilket år er du født? ");
+            var age = 2023 - birthYear;
+
+            var hadBirthday = MyConsole.AskForBool("Har du hatt bursdag i år? (j/n)");
+            if (!hadBirthday)
+            {
+                age--;
+            }
+
+            Console.WriteLine($"Da er du {age} år gammel.");
+        }
+
+        static bool Something()
+        {
+            int i = 5;
+            if (i < 10)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        //for (var i = 0; i < arguments.Length; i++)
+        //{
+        //    var value = arguments[i];
+        //    Console.WriteLine($"arguments[{i}] = \"{value}\"");
+        //}
+
+        //var i = 5;
+        //var s = "Terje";
+
+
+        /*
+         * string
+         * char
+         * int
+         * bool
+         */
+    }
+}
